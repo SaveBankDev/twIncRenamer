857eb7fe911d88419279f01c31c1152a91e5b0f9
SaveBank <60046427+SaveBankDev@users.noreply.github.com>
2026-02-17 16:12:51 +0100

    Added TW comment

--- incRenamerBeta.js
+++ incRenamerBeta.js
@@ -9,6 +9,10 @@
 * Mod: RedAlert
 */
 
+/*
+By uploading a user-generated mod (script) for use with Tribal Wars, the creator grants InnoGames a perpetual, irrevocable, worldwide, royalty-free, non-exclusive license to use, reproduce, distribute, publicly display, modify, and create derivative works of the mod. This license permits InnoGames to incorporate the mod into any aspect of the game and its related services, including promotional and commercial endeavors, without any requirement for compensation or attribution to the uploader. The uploader represents and warrants that they have the legal right to grant this license and that the mod does not infringe upon any third-party rights.
+*/
+
 // User Input
 if (typeof DEBUG !== 'boolean') DEBUG = false;
 if (typeof QUICKSTART !== 'boolean') QUICKSTART = false;
@@ -111,7 +115,7 @@ $.getScript(`https://cdn.jsdelivr.net/gh/SaveBankDev/Tribal-Wars-Scripts-SDK@mai
         (async function () {
             try {
                 const startTime = performance.now();
-                if(QUICKSTART) {
+                if (QUICKSTART) {
                     renameLabels();
                 } else {
                     renderUI();
